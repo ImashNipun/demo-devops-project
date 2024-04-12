c389954cd6275510adcdeba08f009735d6a9791b
Imash Nipun <it21034404@my.sliit.lk>
2024-04-13 04:52:09 +0530

    Add logic to validate and configure .env

---
+++ src/config/index.js
@@ -0,0 +1,30 @@
+const {Joi} = require('celebrate')
+const dotenv = require('dotenv')
+
+dotenv.config()
+class EnvConfig{
+    static get schema(){
+        return{
+            PORT:Joi.number().optional(),
+            MONGO_DB_URI:Joi.string().required()
+        }
+    }
+
+    static get values(){
+        return{
+            PORT:process.env.PORT,
+            MONGO_DB_URI:process.env.MONGO_DB_URI
+        }
+    }
+}
+
+const configurations = EnvConfig.values
+
+const {error} = Joi.object(EnvConfig.schema).validate(configurations)
+
+if(error){
+    console.error(error.details[0].message);
+    process.exit(0);
+}
+
+module.exports = configurations
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const config = require('./config/index')
 
 const app = express()
 
